e8b14a85200b292f3227cf4134d6ca6f94ddb051
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-06 13:31:11 -0500

    up_05

--- Backend/Exmaples/LoginSys.go
+++ Backend/Exmaples/LoginSys.go
@@ -72,3 +72,55 @@ func submitHandler(w http.ResponseWriter, r *http.Request) {
  If there is an error in the connection, an HTTP 500 error is generated with the http.Error function and the request is not responded to.
  The form data is parsed with the r.FormValue() function and a person variable is created using the structure named Person.
  The db.Prepare() function prepares an SQL statement in the database.*/
+
+
+
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rows, err := db.Query("SELECT name, email, message FROM people")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var people []Person
+	for rows.Next() {
+		var name, email, message string
+		if err := rows.Scan(&name, &email, &message); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		person := Person{Name: name, Email: email, Message: message}
+		people = append(people, person)
+	}
+
+	tmpl, err := template.ParseFiles("view.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, people)
+}
+
+
+/* Using the SQL query SELECT name, email, message FROM people, 
+ this code selects data from the people table and converts that data into a slice of Person structures ([]Person).
+ It then displays the data in an HTML page, using the view.html file as the HTML template.
+ The SQL statement (SELECT name, email, message FROM people) used in this code selects all data. 
+ In real-world scenarios, queries will often be more specific and the WHERE statement can be used to select, for example, only a specific user's data.
+
+
+*/
