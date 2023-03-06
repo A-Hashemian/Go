b37e9201a60d5307f9adc3b192a721600daa2767
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-06 13:05:55 -0500

    up_02

--- Backend/Exmaples/LoginSys.go
+++ Backend/Exmaples/LoginSys.go
@@ -8,3 +8,11 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 //These lines are importing the packages we will be using and the SQLite database driver.
+
+type Person struct {
+	Name    string
+	Email   string
+	Message string
+}
+
+//We define a structure named Person, where there are three properties named Name, Email and Message.
