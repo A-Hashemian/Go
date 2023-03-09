fdd74c221ef6f43bb3245861d3ca59ec17328a54
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-09 09:48:22 -0500

    Update HellloWorld.go

--- Backend/Exmaples/HellloWorld.go
+++ Backend/Exmaples/HellloWorld.go
@@ -10,3 +10,16 @@ import(
 type Welcome struct {
 	Name string
 }
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		welcome := Welcome{Name: "Guest"}
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.Execute(w, welcome)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
