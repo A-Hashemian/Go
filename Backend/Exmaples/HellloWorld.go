096aff6e99381a399b01c5e81c298a9a74e41e35
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-09 09:59:20 -0500

    Update HellloWorld.go

--- Backend/Exmaples/HellloWorld.go
+++ Backend/Exmaples/HellloWorld.go
@@ -23,3 +23,30 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 }
+
+/*
+In this code a struct named Welcome is defined and inside this struct there is a variable named Name  This variable will be used to hold the user's name.
+In the main function a function is defined with http.HandleFunc that will run when a request comes to the "/" path. 
+In this function an instance is created from the Welcome struct and the Name variable is assigned the value "Guest" by default. 
+Then, using the r.FormValue function, the "name" value from the form is retrieved and if this value is not empty, this value is assigned to the variable Name.
+
+
+The index.html file is as follows:
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+	<h1>Hello, {{.Name}}!</h1>
+	<form action="/" method="POST">
+		<label for="name">Enter your name:</label>
+		<input type="text" name="name" id="name" />
+		<button type="submit">Submit</button>
+	</form>
+</body>
+</html>
+
+
+*/
