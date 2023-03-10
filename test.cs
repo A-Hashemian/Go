2fb521b35d66e27c0985867bc718b27ccf47ceb6
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-03-10 06:57:30 -0500

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,1 +1,16 @@
 
+Console.Write("Bir sayı girin: ");
+int sayi = int.Parse(Console.ReadLine());
+
+// 4'ün katlarından en yakınını hesapla
+int enYakinKat = sayi - (sayi % 4);
+
+// Eğer en yakın 4'ün katı 10'dan küçük veya eşitse, ekrana yazdır
+if (enYakinKat <= 10)
+{
+    Console.WriteLine(enYakinKat);
+}
+else
+{
+    Console.WriteLine("Girilen sayıya en yakın 4'ün katı 10'dan büyük.");
+}
