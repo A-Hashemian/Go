52cb63f540bf5c9a7121eb1c7b1ecd1ef7b3a96f
A-Hashemian <90988199+A-Hashemian@users.noreply.github.com>
2023-06-17 14:08:15 -0400

    Update ServerConnection.go

--- Backend/Exmaples/Server_Connection/ServerConnection.go
+++ Backend/Exmaples/Server_Connection/ServerConnection.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+)
+
+func handleConnection(conn net.Conn, port string) {
+	defer conn.Close()
+
+	// İstemciden gelen veriyi al
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		log.Println("Veri okuma hatası:", err)
+		return
+	}
